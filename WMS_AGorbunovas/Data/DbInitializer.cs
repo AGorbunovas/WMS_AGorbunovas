4d397bc1134a74a73a2651af134f24f3a5fdc808
Andriejus <a.gorbunovas@gmail.com>
2020-11-25 11:41:25 +0200

    Add project files.

---
+++ WMS_AGorbunovas/Data/DbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WMS_AGorbunovas.Models;
+
+namespace WMS_AGorbunovas.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(AppDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Customer.Any())
+            {
+                return;
+            }
+
+            var customers = new Customer[]
+                 {
+                     new Customer
+                     {
+                         FirstName = "Kaulas",
+                         LastName = "Kaulevičius",
+                         BirthDate = DateTime.Parse("1980-08-10"),
+                         PhoneNumber = 865264526
+                     }
+                 };
+            foreach (Customer customer in customers)
+            {
+                context.Customer.Add(customer);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ WMS_AGorbunovas/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WMS_AGorbunovas.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        public int PhoneNumber { get; set; }
+
+    }
+}
